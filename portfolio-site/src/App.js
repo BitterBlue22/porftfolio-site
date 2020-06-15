a0b777554fa2cb7f11350edb00cc1c648c742d49
BitterBlue22 <telisax@gmail.com>
2020-06-15 16:21:42 +0100

    sets up base App

---
+++ portfolio-site/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import "./App.css";
+
+function App() {
+  return (
+    <CssBaseline/>
+      <header className="App-header"></header>
+    </div>
+  );
+}
+
+export default App;
