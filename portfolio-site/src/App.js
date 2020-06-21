7e32313e1747a9627a337798211e8b6c78c380d4
BitterBlue22 <telisax@gmail.com>
2020-06-21 19:07:27 +0100

    adds initial components

--- portfolio-site/src/App.js
+++ portfolio-site/src/App.js
@@ -1,13 +1,27 @@
 import React from "react";
-import CssBaseline from '@material-ui/core/CssBaseline';
-
+import CssBaseline from "@material-ui/core/CssBaseline";
 import "./App.css";
+import SearchAppBar from "./components/re-useable/NavBar";
+import { Router } from "@reach/router";
+import Home from "./components/Home";
+import Blog from "./components/Blog";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import ContactMe from "./components/ContactMe";
 
 function App() {
   return (
-    <CssBaseline/>
-      <header className="App-header"></header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <SearchAppBar />
+      <Router>
+        <Home path="/" />
+        <AboutMe path="/about-me" />
+        <Projects path="/projects" />
+        <ContactMe path="/contact-me" />
+        <Blog path="/blog" />
+      </Router>
+    </React.Fragment>
   );
 }
 
