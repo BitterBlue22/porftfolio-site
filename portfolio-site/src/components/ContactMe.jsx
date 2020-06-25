4817b0cfa77bc03c5ff4cfafd1d63fcac6ea4a62
BitterBlue22 <telisax@gmail.com>
2020-06-25 10:36:50 +0100

    adds basic contact form fields

--- portfolio-site/src/components/ContactMe.jsx
+++ portfolio-site/src/components/ContactMe.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import FormPropsTextFields from "./re-useable/Form";
 
 const ContactMe = () => {
-  return <form></form>;
+  return (
+    <form>
+      <FormPropsTextFields />
+    </form>
+  );
 };
 
 export default ContactMe;
---
+++ portfolio-site/src/components/re-useable/Form.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+}));
+
+export default function FormPropsTextFields() {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <TextField
+        required
+        id="outlined-required"
+        label="Name"
+        variant="outlined"
+      />
+
+      <TextField
+        required
+        id="outlined-email"
+        label="Email"
+        type="email"
+        variant="outlined"
+      />
+
+      <TextField
+        id="outlined-message"
+        label="Message"
+        helperText="How can I help?"
+        variant="outlined"
+      />
+      <Button>Submit</Button>
+    </form>
+  );
+}
