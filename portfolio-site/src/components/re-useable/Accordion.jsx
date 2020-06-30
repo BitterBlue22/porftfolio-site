b8d62bf5d428352cb8a30070e3d8fe9bf0a16783
BitterBlue22 <telisax@gmail.com>
2020-06-30 10:50:29 +0100

    updates links to buttons

--- portfolio-site/src/components/re-useable/Accordion.jsx
+++ portfolio-site/src/components/re-useable/Accordion.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { styled } from "@material-ui/core/styles";
-import ExpansionPanel from "@material-ui/core/ExpansionPanel";
-import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
-import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
-import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import {
+  Button,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+  Typography,
+  styled,
+} from "@material-ui/core";
 
 const StyledExpansionPanel = styled(ExpansionPanel)({
   background: "#94618E",
@@ -19,7 +22,6 @@ const StyledExpansionPanelDetails = styled(ExpansionPanelDetails)({
   backgroundColor: "#FFFFFF",
   border: 0,
   borderRadius: 3,
-
   color: "#004c4c",
   padding: "20px",
   width: "100%",
@@ -35,6 +37,21 @@ const StyledTypography = styled(Typography)({
   fontFamily: "Work Sans",
   fontSize: "large",
   textAlign: "justify",
+});
+const StyledButton = styled(Button)({
+  background: "#94618E",
+  border: 0,
+  borderRadius: 3,
+  boxShadow: "0 3px 5px 2px #49274A",
+  color: "white",
+  height: 48,
+  padding: "0 30px",
+  margin: "10px",
+  "&:hover": {
+    background: "	#49274A",
+    borderRadius: 3,
+    boxShadow: "0 3px 5px 2px #94618E",
+  },
 });
 
 export default function StyledComponents() {
@@ -64,42 +81,55 @@ export default function StyledComponents() {
           <StyledTypography>
             Ever have a hard time deciding where to go for dinner? Try Chicken
             Tinder, a fun way to choose where to eat!
-            <section>
-              <h4>Frontend:</h4>
-              <a href=" https://chicken-tinder-nc.netlify.app/  ">Hosted</a>
-              <br />
-              <a href="https://github.com/hayeskg/ChickenTinderFrontend  ">
-                Github
-              </a>
-              <h6>Tech stack:</h6>
-              <ul>
-                <li>JavaScript</li>
-                <li>HTML5/CSS3</li>
-                <li>React</li>
-                <li>GraphQL (and Apollo)</li>
-                <li>MaterialUI</li>
-                <li>Firebase Authentication</li>
-              </ul>
-            </section>
-            <section>
-              <h4>Backend:</h4>
-              <a href="https://chicken-tinder-backend.herokuapp.com/graphql">
-                Hosted
-              </a>
-              <br />
-              <a href="https://github.com/hayeskg/ChickenTinderBackend">
-                Github
-              </a>
-              <h6>Tech stack:</h6>
-              <ul>
-                <li>JavaScript</li>
-                <li>MongoDB Atlas (and mongoose)</li>
-                <li>Express</li>
-                <li>GraphQL</li>
-                <li>Axios</li>
-                <li>Jest</li>
-              </ul>
-            </section>
+            <h4>Frontend:</h4>
+            <a href="https://chicken-tinder-nc.netlify.app/" className="link">
+              <StyledButton>
+                <span>Hosted</span>
+              </StyledButton>
+            </a>
+            <a
+              href="https://github.com/hayeskg/ChickenTinderFrontend"
+              className="link"
+            >
+              <StyledButton>
+                <span>GitHub</span>
+              </StyledButton>
+            </a>
+            <h5>Tech stack:</h5>
+            <ul>
+              <li>JavaScript</li>
+              <li>HTML5/CSS3</li>
+              <li>React</li>
+              <li>GraphQL (and Apollo)</li>
+              <li>MaterialUI</li>
+              <li>Firebase Authentication</li>
+            </ul>
+            <h4>Backend:</h4>
+            <a
+              href="https://chicken-tinder-backend.herokuapp.com/graphql"
+              className="link"
+            >
+              <StyledButton>
+                <span>Hosted</span>
+              </StyledButton>
+            </a>
+            <a
+              href="https://github.com/hayeskg/ChickenTinderBackend"
+              className="link"
+            >
+              <StyledButton>
+                <span>GitHub</span>
+              </StyledButton>
+            </a>
+            <h5>Tech stack:</h5>
+            <ul>
+              <li>JavaScript</li>
+              <li>MongoDB Atlas (and mongoose)</li>
+              <li>Express</li>
+              <li>GraphQL</li>
+              <li>Axios</li>
+              <li>Jest</li>
+            </ul>
           </StyledTypography>
         </StyledExpansionPanelDetails>
       </StyledExpansionPanel>
@@ -114,35 +144,48 @@ export default function StyledComponents() {
         <StyledExpansionPanelDetails>
           <StyledTypography>
             A Reddit-inspired news site
-            <section>
-              <h4>Frontend:</h4>
-              <a href="https://tel-nc-news.netlify.app/ ">Hosted</a>
-              <br />
-              <a href="https://github.com/BitterBlue22/NC-news-FE ">Github</a>
-              <h6>Tech stack:</h6>
-              <ul>
-                <li>JavaScript</li>
-                <li>HTML5/CSS3</li>
-                <li>React</li>
-                <li>@reach/router</li>
-                <li>Styled Components</li>
-                <li>Axios</li>
-              </ul>
-            </section>
-            <section>
-              <h4>Backend:</h4>
-              <a href="https://tel-news-port.herokuapp.com/api">Hosted</a>
-              <br />
-              <a href="https://github.com/BitterBlue22/NC_News">Github</a>
-              <h6>Tech stack:</h6>
-              <ul>
-                <li>JavaScript</li>
-                <li>Express</li>
-                <li>PostgreSQL</li>
-                <li>Knex.js</li>
-                <li>Jest</li>
-              </ul>
-            </section>
+            <h4>Frontend:</h4>
+            <a href="https://tel-nc-news.netlify.app/ " className="link">
+              <StyledButton>
+                <span>Hosted</span>
+              </StyledButton>
+            </a>
+            <a
+              href="https://github.com/BitterBlue22/NC-news-FE "
+              className="link"
+            >
+              <StyledButton>
+                <span>GitHub</span>
+              </StyledButton>
+            </a>
+            <h5>Tech stack:</h5>
+            <ul>
+              <li>JavaScript</li>
+              <li>HTML5/CSS3</li>
+              <li>React</li>
+              <li>@reach/router</li>
+              <li>Styled Components</li>
+              <li>Axios</li>
+            </ul>
+            <h4>Backend:</h4>
+            <a href="https://tel-news-port.herokuapp.com/api" className="link">
+              <StyledButton>
+                <span>Hosted</span>
+              </StyledButton>
+            </a>
+            <a href="https://github.com/BitterBlue22/NC_News" className="link">
+              <StyledButton>
+                <span>GitHub</span>
+              </StyledButton>
+            </a>
+            <h5>Tech stack:</h5>
+            <ul>
+              <li>JavaScript</li>
+              <li>Express</li>
+              <li>PostgreSQL</li>
+              <li>Knex.js</li>
+              <li>Jest</li>
+            </ul>
           </StyledTypography>
         </StyledExpansionPanelDetails>
       </StyledExpansionPanel>
